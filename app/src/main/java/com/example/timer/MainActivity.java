f780aa80b4f1c4162aca31ff45d6411a4f6069d6
Stanislav55 <info7762@gmail.com>
2020-04-19 14:57:19 +0600

    Step_3

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.timer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -116,4 +121,27 @@ public class MainActivity extends AppCompatActivity {
         isTimerOn = false;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.timer_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent openSettings =
+                    new Intent(this, SettingsActivity.class);
+            startActivity(openSettings);
+            return true;
+        } else if (id == R.id.action_about) {
+            Intent openAbout =
+                    new Intent(this, AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }
+            return super.onOptionsItemSelected(item);
+    }
 }
